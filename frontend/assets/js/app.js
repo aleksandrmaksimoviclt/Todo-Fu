4aeb4def27cd47542bf19b352efa9fe82b0c1c40
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2016-12-06 17:53:47 +0100

    object to array module added

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,4 +1,5 @@
 var app = angular.module('todofu', [
+    'angular-toArrayFilter',
     'ngAnimate',
     'ui.router'
 ]);
@@ -82,6 +83,25 @@ app.controller('cardComposerController', function($scope){
         setTimeout(setFocus, 1)
     };
 });
+app.constant('BASE_URL', 'http://localhost:8000/api/todos/');
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'assets/home/home.template.html',
+            controller: 'homeController'
+        });
+        // .state('home.add-todo', {
+        //     url: 'add-todo',
+        //     templateUrl: 'assets/home/add-todo.template.html',
+        //     controller: 'homeController'
+        // });
+
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
+});
 app.service('Todos', function($http, BASE_URL){
     var Todos = {};
 
@@ -103,23 +123,4 @@ app.service('Todos', function($http, BASE_URL){
 
     return Todos;
 
-});
-app.constant('BASE_URL', 'http://localhost:8000/api/todos/');
-
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
-
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'assets/home/home.template.html',
-            controller: 'homeController'
-        });
-        // .state('home.add-todo', {
-        //     url: 'add-todo',
-        //     templateUrl: 'assets/home/add-todo.template.html',
-        //     controller: 'homeController'
-        // });
-
-    $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise('/');
 });
